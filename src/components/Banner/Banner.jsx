328177be0e2bcc80e40cc6410d88e78bf49af33e
sahin404 <sahinraj20@gmail.com>
2024-07-28 23:00:40 +0600

    banner section done

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,10 +1,26 @@
+import "../../index.css";
 const Banner = () => {
-    return (
-        <div className="mt-20 text-3xl text-center">
-            Banner Section
-
+  return (
+    <div className="bg-image">
+      <div className="overlay flex items-center justify-center">
+        <div>
+          <h1 className="font-bold text-4xl">
+            I Grow By Helping People In Need
+          </h1>
+          <div className="text-center mt-5 flex justify-center">
+            <input
+              className="w-3/5 bg-transparent border border-black px-3 py-2 rounded-l-lg"
+              placeholder="Search here.."
+              type="text"
+            />
+            <div className="w-1/5 px-3 py-2 rounded-r-lg bg-orange-600">
+              Search
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
